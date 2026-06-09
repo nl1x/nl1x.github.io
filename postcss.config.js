0fa0a2c5452b052379c15449b9227460d186e474
nl1x <nathan.jeannot@epitech.eu>
2026-06-09 21:32:20 +0200

    fix(style): fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,10 @@ export default {
     'postcss-custom-media': {}, // Allow var in media-queries
     'postcss-nesting': {},      // Allows nesting css
     autoprefixer: {},           // Gère la compatibilité navigateurs
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {
+        preset: ['default', {
+          autoprefixer: false // Fix the backdrop-filter
+        }]
+      } } : {})
   },
 }
